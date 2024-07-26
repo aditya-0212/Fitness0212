2daf012f2a8bf38d7f790918a5eff6ccb6d05fb4
Aditya Choubisa <adityachoubisa02@gmail.com>
2024-07-26 11:05:16 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by APPLE on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
