0f7df37ff0aedc275206a3ec24bd057a8620e359
Aditya Choubisa <adityachoubisa02@gmail.com>
2024-07-26 12:59:55 +0530

    sgdf

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello Aditya...you are going to be ")
+            Text("Hello Aditya...you are going to be ios developer and you will do best")
         }
         .padding()
     }
